e1d32da588a5a4f0daccda5b563ddf2b17ce16ac
base1551 <baseball.1.kyoto@gmail.com>
2022-05-28 15:39:24 +0900

    数値増加のonClick関数実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+//useStateは関数コンポーネントに状態を持たせるためのReactの機能
+import React, {useState} from "react";
 import "./App.css";
 
 function App() {
-  return (
-      <div className="App">
-          <header className="App-header">
-              <LikeButton />
-          </header>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <LikeButton/>
+            </header>
+        </div>
+    );
 }
 
 function LikeButton() {
-    const count = 999;
-    return <span className="likeButton">♥ {count}</span>;
+    const [count, setCount]= useState(999);
+    const handleClick = () => {
+        setCount(count + 1);
+    }
+    return <span className="likeButton" onClick={handleClick}>♥ {count}</span>;
 }
+
 export default App;
